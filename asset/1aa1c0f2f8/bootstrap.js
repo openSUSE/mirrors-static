6139fad98f7bf90fc04c0e43af3115aab2f65548
mirrors_static <mirrors_static@narwal5.infra.opensuse.org>
2024-04-04 10:55:01 +0000

    Update via cronjob at 2024-04-04 10:55

--- asset/1aa1c0f2f8/bootstrap.js
+++ asset/1aa1c0f2f8/bootstrap.js
@@ -1958,14 +1958,13 @@ function checkFileOnMirrors(path){$.ajax({url:'/rest/server/check_file?file='+pa
 function handleCheckFileOnMirror(job_id){$(location).attr('href','/minion/jobs?id='+job_id);}
 function setupReportTable(){var columns=[];columns.push({data:'region',defaultContent:""});columns.push({data:'country',defaultContent:""});columns.push({data:'url',defaultContent:""});$('#checkboxes label').each(function(){var columnName=$(this).text();if(columnName=='Blame'){return;}
 columnName=columnName.trim().replace(/ /g,"").toLowerCase().replace(/\./g,"");if(columnName.match(/^\d/)){columnName='c'+columnName;}
-columns.push({data:(columnName+'score'),defaultContent:""});columns.push({data:(columnName+'victim'),defaultContent:""});});var url=$("#reporttable_api_url").val();var table=$('.reporttable');var dataTable=table.DataTable({order:[[0,'asc']],ajax:{url:url,dataSrc:'report'},columns:columns,search:{regex:true,},});dataTable.rowData=[];$('#checkboxes').on('change',':checkbox',function(){layoutReportTable();});layoutReportTable();}
-function layoutReportTable(){var dt=$('.reporttable').DataTable();var victim=1;if(!$('#victimcheckbox').is(":checked")){victim=0;}
-var existchecked=0;$('#checkboxes label').each(function(){var columnName=$(this).text();if(columnName=='Blame'){return;}
+columns.push({data:(columnName+'score'),defaultContent:""});});var url=$("#reporttable_api_url").val();var table=$('.reporttable');var dataTable=table.DataTable({order:[[0,'asc']],ajax:{url:url,dataSrc:'report'},columns:columns,search:{regex:true,},});dataTable.rowData=[];$('#checkboxes').on('change',':checkbox',function(){layoutReportTable();});layoutReportTable();}
+function layoutReportTable(){var dt=$('.reporttable').DataTable();var existchecked=0;$('#checkboxes label').each(function(){var columnName=$(this).text();if(columnName=='Blame'){return;}
 var vis=1;if($("[ id='"+columnName+"checkbox' ]").is(":checked")){existchecked=1;}});var i=0;var firstColumnHack=0;var firstColumnIndex=3;$('#checkboxes label').each(function(){var lbl=$(this);var columnName=lbl.text();if(columnName=='Blame'){return;}
 var vis=1;if(existchecked&&!$("[ id='"+columnName+"checkbox' ]").is(":checked")){vis=0;}
 if(i==0){firstColumnHack=vis;}
 var index=firstColumnIndex+2*i;if(vis!=dt.columns(index).visible()){dt.columns(index).visible(vis);}
-if(vis&&victim!=dt.columns(index+1).visible()){dt.columns(index+1).visible(victim);}else if(!vis&&dt.columns(index+1).visible()){dt.columns(index+1).visible(vis);}
+if(!vis&&dt.columns(index+1).visible()){dt.columns(index+1).visible(vis);}
 i=i+1;});dt.columns(firstColumnIndex).visible(firstColumnHack);}
 function setupReportDownloadTable(column){var columns=[];var keys=column.split(',');columns.push({data:'dt',defaultContent:"",render:function(data){var date=new Date(data);return date.toLocaleDateString();}});columns.push({data:column,defaultContent:"",render:function(data,type,row,meta){data="";for(let i=0;i<keys.length;i++){if(data!=""){data=data+',';}
 data=data+row[keys[i]];}
